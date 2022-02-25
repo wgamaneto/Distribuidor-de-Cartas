1a9c141cae1d15800d25df13c2051484dd8db3ee
wgamaneto <96584618+wgamaneto@users.noreply.github.com>
2022-02-25 20:17:50 -0300

    Add files via upload

---
+++ Destribuidor de cartas de baralho/cartas1.py
@@ -0,0 +1,30 @@
+import random
+
+
+NAIPES = '♠ ♡ ♢ ♣'.split()
+CARTAS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
+
+
+def criar_baralho(aleatorio=False):
+    baralho = [(n, c) for c in CARTAS for n in NAIPES]
+    if aleatorio:
+        random.shuffle(baralho)
+    return baralho
+
+
+def distribuir_cartas(baralho):
+    return (baralho[0::4], baralho[1::4], baralho[2::4], baralho[3::4])
+
+
+def jogar():
+    cartas = criar_baralho(aleatorio=True)
+    jogadores = 'P1 P2 P3 P4'.split()
+    maos = {j: m for j, m in zip(jogadores, distribuir_cartas(cartas))}
+
+    for jogador, cartas in maos.items():
+        carta = ' '.join(f"{j}{c}" for (j, c) in cartas)
+        print(f'{jogador}: {cartas}')
+
+
+if __name__ == '__main__':
+    jogar()
